ad5791d9ae974826e7071c25379e59cf35b2f96f
EyCiYo <alexchdn12@gmail.com>
2023-03-11 19:50:06 +0530

    Changed Body Type

--- Levels/levelStack.gd
+++ Levels/levelStack.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 var stack_item = preload("res://Reusable/wood_plank.tscn")
+var available_log = $logAvailable.get_text()
 var current_block_loc = $woodPlank.position
-var available_log = int($logAvailable.text)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,12 +16,12 @@ func _process(delta):
 
 func _on_button_button_down():
 	print("Button Pressed")
-	available_log -= 1
 	print(available_log)
 	spawn_block(current_block_loc)
 
 
 func spawn_block(spawn_location):
 	var instance = stack_item.instance()
-	instance.global_position = spawn_location
+	instance.global_position.x = spawn_location.x
+	instance.global_position.y = spawn_location.y + 1
 	add_child(instance)
