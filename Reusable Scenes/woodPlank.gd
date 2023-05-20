f82128fa438f0a27d998e55ba0668afb56dc4a13
ARJ18 <arjunb1802@gmail.com>
2023-05-20 11:30:33 +0530

    Added levelgraph

--- Reusable Scenes/woodPlank.gd
+++ Reusable Scenes/woodPlank.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-var plank_location
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,5 +11,3 @@ func _process(delta):
 	pass
 
 
-func _on_woodPlank_mouse_entered():
-	plank_location = get_global_mouse_position()
