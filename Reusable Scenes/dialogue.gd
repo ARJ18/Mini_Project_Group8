0db4a20f759dc8fe0ec5b6215b714040204372f8
EyCiYo <alexchdn12@gmail.com>
2023-06-07 13:05:09 +0530

    dialogue and stacktutorial changes

--- Reusable Scenes/dialogue.gd
+++ Reusable Scenes/dialogue.gd
@@ -7,16 +7,20 @@ var curr_dia_id = -1
 
 func _ready():
 	start()
-	$AnimationPlayer.play("TextAnim")
 
 func start():
 	dialouge = load_dialogue()
+	next_script()
 	
 func load_dialogue():
 	var file = File.new()
 	if file.file_exists(d_file):
 		file.open(d_file,File.READ)
 		return parse_json(file.get_as_text())
+
+func _input(event):
+	if event.is_action_pressed("ui_click"):
+		next_script()
 
 func next_script():
 	curr_dia_id += 1
---
+++ Scenes/stackTutorial.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _ready():
+	$player.set_physics_process(false)
