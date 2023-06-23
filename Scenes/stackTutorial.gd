1ad0c0df82652d1833c7a64cf9dc1e31a219d447
EyCiYo <alexchdn12@gmail.com>
2023-06-23 22:42:46 +0530

    stacktutorial changes

--- Scenes/stackTutorial.gd
+++ Scenes/stackTutorial.gd
@@ -19,6 +19,7 @@ func _ready():
 	$AnimElems/PushBtn.visible = false
 	$AnimElems/PushAlgo.visible = false
 	$AnimElems/Marker.visible = false
+	$AnimElems/PopBtn.visible = false
 	
 func _process(delta):
 	$Label.text = str($dialogue.curr_dia_id)
@@ -39,18 +40,29 @@ func handle_anim():
 	if $dialogue.curr_dia_id == 4:
 		anim.play("Push4")
 	if $dialogue.curr_dia_id == 5:
-		$AnimElems/PushBtn.visible = true
+		$AnimElems/PushBtn.visible = true		#push button visible
 		$dialogue.get_node("goback").disabled = true
 		$dialogue.get_node("gofront").disabled = true
 	if $dialogue.curr_dia_id == 6:
-		anim.play("PushAlgoAppear")
+		anim.play("PushAlgoAppear")				#push algo appear
 	if $dialogue.curr_dia_id == 7:
-		anim.play("OverflowHighlight")
+		anim.play("OverflowHighlight")			#condition check highlight
 	if $dialogue.curr_dia_id == 8:
-		anim.play("TopAddHighlight")
+		anim.play("TopAddHighlight")			#pushing highlight
 		anim.queue("OverflowHighlight")
-		
-
+	if $dialogue.curr_dia_id == 9:
+		$AnimElems/PushBtn.visible = false		#remove all push elements
+		$AnimElems/PushAlgo.visible = false
+		$AnimElems/Marker.visible = false
+		anim.stop(true)
+	if $dialogue.curr_dia_id == 10:
+		var text = $AnimElems/StackPancake5.get_node("StackId")
+		text.add_color_override("font_color",Color.red)
+		anim.play("Stack5Highlight")			#stack5 scale anim
+	if $dialogue.curr_dia_id == 11:
+		$AnimElems/PopBtn.visible = true		#introduce pop button
+		$dialogue.get_node("goback").disabled = true
+		$dialogue.get_node("gofront").disabled = true
 
 func handle_backward_anim():
 	if $dialogue.curr_dia_id == 0:
@@ -58,36 +70,74 @@ func handle_backward_anim():
 	if $dialogue.curr_dia_id == 1:
 		anim.play_backwards("TopAppear")
 	if $dialogue.curr_dia_id == 2:
-		anim.play_backwards("TopEmph")
+		anim.seek(0.00,true)
+		anim.stop(true)
 	if $dialogue.curr_dia_id == 3:
 		anim.play("Pop4")
+		anim.queue("TopEmph")
 	if $dialogue.curr_dia_id == 4:
 		if is_pushed:
-			anim.play("pop5")
-			stack_top -= 1
+			pop5()
 		$AnimElems/PushBtn.visible = false
 	if $dialogue.curr_dia_id == 5:
-		anim.play("pop5")
-		is_pushed = false
-		stack_top-=1
+		pop5()
 		$AnimElems/PushAlgo.visible = false
+		$dialogue.get_node("goback").disabled = true
+		$dialogue.get_node("gofront").disabled = true
 	if $dialogue.curr_dia_id == 6:
+		anim.seek(0.00,true)
 		anim.stop(true)
 		$AnimElems/Marker.visible = false
 	if $dialogue.curr_dia_id == 7:
-		anim.play_backwards("TopAddHighlight")
+		anim.play_backwards("TopAddHighlight")		#pushing part highlight
 		anim.queue("OverflowHighlight")
+	if $dialogue.curr_dia_id == 8:
+		$AnimElems/PushBtn.visible = true
+		$AnimElems/PushAlgo.visible = true
+		$AnimElems/Marker.visible = true			#bring back push elements
+		anim.queue("OverflowHighlight")
+	if $dialogue.curr_dia_id == 9:
+		anim.seek(0.00,true)
+		anim.stop(true)
+		var text = $AnimElems/StackPancake5.get_node("StackId")
+		text.add_color_override("font_color",Color.black)
+	if $dialogue.curr_dia_id == 10:
+		anim.seek(0.00,true)
+		anim.stop(true)
+		if !is_pushed:
+			push5()
+		anim.queue("Stack5Highlight")
+		var text = $AnimElems/StackPancake5.get_node("StackId")
+		text.add_color_override("font_color",Color.red)	#remove stack5 color
+	if $dialogue.curr_dia_id == 11:
+		push5()
 
 func _on_gohomeBtn_button_up():
 	get_tree().change_scene("res://Scenes/levelSelector.tscn")
 
+func push5():
+	anim.play("Push5")
+	is_pushed = true
+	stack_top += 1
 
+func pop5():
+	anim.play("pop5")
+	is_pushed = false
+	stack_top -= 1
+	
 func _on_PushBtn_button_up():
 	if stack_top == 5:
 		print("Overflow")
 	else:
-		anim.play("Push5")
-		is_pushed = true
-		stack_top += 1
+		push5()
+	$dialogue.get_node("goback").disabled = false
+	$dialogue.get_node("gofront").disabled = false
+
+
+func _on_PopBtn_button_up():
+	if stack_top == 5:
+		pop5()
+	else:
+		print("Underflow")
 	$dialogue.get_node("goback").disabled = false
 	$dialogue.get_node("gofront").disabled = false
